c75ca4cc856590301558d0e4a0923b7fa0314654
muzhial <kantchenjn@163.com>
2022-02-10 11:23:37 +0000

    nullptr

--- pytorch_kit/loss_found.py
+++ pytorch_kit/loss_found.py
@@ -1,6 +1,54 @@
 import numpy as np
 import torch
 import torch.nn as nn
+from torch.nn import functional as F
+
+
+def test_LogSoftmax():
+    v = torch.tensor([[1, 2, 3],
+                      [2, 3, 4]], dtype=torch.float64)
+    log_softmax = nn.LogSoftmax(dim=1)
+    p1 = log_softmax(v)
+
+    # equal
+    p = torch.exp(v)
+    p_s = p.sum(dim=1, keepdim=True)
+    p2 = (p / p_s).log()
+
+    assert torch.equal(log_softmax(v), p)
+
+
+def test_NLLLoss():
+    pred = torch.tensor([[0.1, 0.2, 0.3],
+                         [0.9, 0.3, 0.6],
+                         [0.1, 0.3, 0.5]])
+    target = torch.tensor([1, 2, 0])
+    l = nn.NLLLoss(reduction='mean')
+    # l = nn.NLLLoss(reduction='mean', ignore_index=2)
+
+    # equal
+    p = pred.gather(1, target.unsqueeze(-1))
+    # res = -1 * p.sum()
+    res = -1 * p.mean()
+    print(l(pred, target))
+    print(res)
+
+
+def test_CrossEntropyLoss():
+    pred = torch.tensor([[0.1, 0.2, 0.3],
+                         [0.9, 0.3, 0.6],
+                         [0.1, 0.3, 0.5]])
+    target = torch.tensor([1, 2, 0])
+
+    cross_entropy_loss = nn.CrossEntropyLoss(ignore_index=2)
+
+    # equal
+    log_softmax = nn.LogSoftmax(dim=1)
+    nllloss = nn.NLLLoss(ignore_index=2)
+    res = nllloss(log_softmax(pred), target)
+
+    print(cross_entropy_loss(pred, target))
+    print(res)
 
 
 def test_BCEWithLogits():
@@ -15,49 +63,9 @@ def test_BCEWithLogits():
     si = si.mean()
     print(si)
 
-def test_():
-    v = torch.tensor([[1, 2, 3],
-                      [2, 3, 4]], dtype=torch.float64)
-    v1 = nn.LogSoftmax(dim=1)
-    print(v1(v))
-
-    p = torch.exp(v)
-    p_s = p.sum(dim=1, keepdim=True)
-    p = (p / p_s).log()
-    print(p)
-
-    v = torch.tensor([[0.1, 0.2, 0.3],
-                      [0.9, 0.3, 0.6],
-                      [0.1, 0.3, 0.5]])
-    v_t = torch.tensor([1, 2, 0])
-
-    # l = nn.NLLLoss(reduction='sum')
-    # l = nn.NLLLoss(reduction='mean', ignore_index=2)
-    l = nn.NLLLoss(reduction='mean')
-    res = l(v, v_t)
-    print(res)
-
-    p = v.gather(1, v_t.unsqueeze(-1))
-    # res = -1 * p.sum()
-    res = -1 * p.mean()
-    print(res)
-
-    l = nn.CrossEntropyLoss(ignore_index=2)
-    res = l(v, v_t)
-    print(res)
-
-    l1 = nn.LogSoftmax()
-    l2 = nn.NLLLoss(ignore_index=2)
-    res = l1(v)
-    res = l2(res, v_t)
-    print(res)
-
-    l = nn.NLLLoss(reduction='none')
-    v1 = torch.rand(2, 3, 5, 5)
-    print(v1)
-    v2 = torch.zeros(2, 5, 5).random_(3).long()
-    print(v2)
-    print(l(v1, v2))
 
 if __name__ == '__main__':
+    # test_LogSoftmax()
+    # test_NLLLoss()
+    # test_CrossEntropyLoss()
     test_BCEWithLogits()
