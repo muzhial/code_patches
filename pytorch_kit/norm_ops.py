75c69fe0f0c57c01033c059a2292521c1438cf81
muzhi <kantchenjn@163.com>
2022-02-14 01:09:25 +0800

    nullptr

--- pytorch_kit/norm_ops.py
+++ pytorch_kit/norm_ops.py
@@ -3,8 +3,18 @@ import torch.nn as nn
 
 
 # stack
-y, x = torch.meshgrid([torch.arange(3), torch.arange(2)])
-print(y)
-print(x)
-z = torch.stack((x, y), dim=2)
-print(z)
+def stack():
+    y, x = torch.meshgrid([torch.arange(3), torch.arange(2)])
+    print(y)
+    print(x)
+    z = torch.stack((x, y), dim=2)
+    print(z)
+
+
+def dim_op():
+    a = torch.randn(3, 2)
+    print(a[:, None].size())
+
+
+if __name__ == '__main__':
+    dim_op()
